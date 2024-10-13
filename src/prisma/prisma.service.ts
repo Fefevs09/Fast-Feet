cb1ed8e16dca41b93db1f9fc66ddff241a829a6e
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-10-13 19:44:31 -0300

    refactor: remove unnecessary `client` class

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,14 +4,12 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService
   extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy {
-  public client: PrismaClient;
-
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
     super({
       log: ['warn', 'error'],
     });
-    this.client = new PrismaClient();
   }
 
   onModuleInit() {
