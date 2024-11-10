1cd9f9e7b5ec1a5a2ea9d44a4e6e22f3e7e156e8
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-11-10 16:28:05 -0300

    feat(api): add swagger api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { Env } from '../src/env';
+import { Env } from './infra/env/env';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     // logger: true,
   });
+  const config = new DocumentBuilder()
+    .setTitle('Fast feet API')
+    .setDescription('Fast feet product transporter')
+    .setVersion('1.0')
+    .addTag('fast')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   const wasValidate: boolean = true;
 
