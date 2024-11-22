fbb8c7f474135e72db88e667118436eea6ac0d89
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-11-21 23:01:50 -0300

    feat(domain): add notification entity

---
+++ src/domain/notification/enterprise/entities/notification.ts
@@ -0,0 +1,48 @@
+import { Entity } from '@/core/entities/entity';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { Optional } from '@/core/types/optional';
+
+interface NotificationProps {
+  recipientId: UniqueEntityID;
+  title: string;
+  content: string;
+  readAt?: Date;
+  createdAt: Date;
+}
+
+export class Notification extends Entity<NotificationProps> {
+  public get recipientId(): UniqueEntityID {
+    return this.props.recipientId;
+  }
+
+  public get title(): string {
+    return this.props.title;
+  }
+
+  public get content(): string {
+    return this.props.content;
+  }
+
+  public get readAt(): Date | undefined {
+    return this.props.readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+
+  static create(
+    props: Optional<NotificationProps, 'createdAt'>,
+    id?: UniqueEntityID,
+  ) {
+    const notification = new Notification(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    );
+
+    return notification;
+  }
+}
